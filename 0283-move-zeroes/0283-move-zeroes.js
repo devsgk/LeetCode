c1ed0d6ae6faa92bb0d329311b4bbd0863b5806c
Seunggyu Kim <devkim0702@gmail.com>
2024-04-25 09:11:40 +0900

    Time: 85 ms (24.59%), Space: 53.5 MB (87.72%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+  let count = 0;
+  
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 0) {
+      count++;
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+
+  while (count !== 0) {
+    nums.push(0);
+    count--;
+  }
+  
+  return nums;
+};

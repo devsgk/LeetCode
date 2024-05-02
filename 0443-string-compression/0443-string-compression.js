2a93b52d44db85aed135f1c53a7c17cc80436d3d
Seunggyu Kim <devkim0702@gmail.com>
2024-05-02 11:00:41 +0900

    Time: 61 ms (65.54%), Space: 50.7 MB (97.85%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.js
@@ -0,0 +1,27 @@
+/**
+ * @param {character[]} chars
+ * @return {number}
+ */
+var compress = function(chars) {
+  let str = "";
+  let count = 1;
+  
+  for (let i = 0; i < chars.length; i++) {
+    if (chars[i] === chars[i + 1]) {
+      count++;
+    } else {
+      if (count === 1) {
+        str += chars[i];
+      } else {
+        str += chars[i] + count;
+      }
+
+      count = 1;
+    }
+  }
+  const result = str.split("");
+
+  chars.splice(0, chars.length, ...result)
+
+  return chars.length;
+};

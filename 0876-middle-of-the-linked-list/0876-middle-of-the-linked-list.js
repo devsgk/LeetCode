31d9c32f70ff210feb5bedf51f9cd377388162ff
Seunggyu Kim <devkim0702@gmail.com>
2024-05-08 09:11:45 +0800

    Time: 47 ms (78.36%), Space: 49.1 MB (22.46%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+  let length = 0;
+  let middleIndex;
+  let cur = head;
+  let count = 0;
+  let answer;
+  
+  while (cur) {
+    cur = cur.next;
+    length++;
+  }
+  
+  middleIndex = length % 2 === 0 ? (length / 2) + 1 : parseInt(length / 2) + 1;
+  console.log(middleIndex);
+  
+  while (count < middleIndex) {
+    answer = head;
+    head = head.next;
+    count++;
+  }
+  
+  return answer;
+};

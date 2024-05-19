a36a0ca76c507c4803bffccff7eb50aa1392df66
Seunggyu Kim <devkim0702@gmail.com>
2024-05-19 09:31:08 +0800

    Time: 80 ms (76.07%), Space: 64.2 MB (33.09%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeStars = function(s) {
+  const stack = [];
+  
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] !== "*") {
+      stack.push(s[i]);
+    } else {
+      stack.pop();
+    }
+  }
+  
+  return stack.join("");
+};

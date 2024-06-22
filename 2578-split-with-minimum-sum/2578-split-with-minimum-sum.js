28df4f85c156b8c764e1bb89033e1897d5cc1a25
Seunggyu Kim <devkim0702@gmail.com>
2024-06-22 22:33:14 +0800

    Time: 54 ms (46.10%), Space: 49.1 MB (39.72%) - LeetHub

--- 2578-split-with-minimum-sum/2578-split-with-minimum-sum.js
+++ 2578-split-with-minimum-sum/2578-split-with-minimum-sum.js
@@ -8,8 +8,11 @@ var splitNum = function(num) {
   let num2 = "";
   
   for (let i = 0; i < arr.length; i++) {
-    if (i % 2 === 0) num1 += arr[i];
-    else num2 += arr[i];
+    if (i % 2 === 0) {
+      num1 += arr[i];
+    } else {
+      num2 += arr[i];
+    }
   }
   
   return Number(num1) + Number(num2);

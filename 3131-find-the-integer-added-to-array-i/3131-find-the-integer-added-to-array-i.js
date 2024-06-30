90971416ea522b7ad73fc4ad8ba36063ca8daaee
Seunggyu Kim <devkim0702@gmail.com>
2024-06-30 19:26:34 +0800

    Time: 63 ms (47.73%), Space: 50 MB (96.88%) - LeetHub

--- 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.js
+++ 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.js
@@ -4,22 +4,5 @@
  * @return {number}
  */
 var addedInteger = function(nums1, nums2) {
-  const obj1 = {};
-  const obj2 = {};
-  let x = 0;
-  
-  for (let i = 0; i < nums1.length; i++) {
-    obj1[nums1[i]] = obj1[nums1[i]] ? obj1[nums1[i]] + 1 : 1
-    obj2[nums2[i]] = obj2[nums2[i]] ? obj2[nums2[i]] + 1 : 1
-  }
-  
-  for (const [key1, value1] of Object.entries(obj1)) {
-    for (const [key2, value2] of Object.entries(obj2)) {
-      if (value1 === value2) {
-        x = Number(key2) - Number(key1);
-        
-        return x;
-      }
-    }
-  }
+  return Math.min(...nums2) - Math.min(...nums1);
 };

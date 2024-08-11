48062e5f14935b49e26ae2bb0e4664bb4d61ca38
Seunggyu Kim <devkim0702@gmail.com>
2024-08-11 22:10:51 +0800

    Time: 46 ms (93.76%), Space: 50.5 MB (30.38%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.js
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.js
@@ -3,9 +3,21 @@
  * @return {number}
  */
 var maxProduct = function(nums) {
-  const arr = nums.slice().sort((a, b) => a - b);
+  let max1 = 0;
+  let max2 = 0;
+  let arr = nums.slice();
+
+  for (const num of arr) {
+    max1 = Math.max(num, max1);
+  }
   
-  console.log(arr);
+  const index = arr.indexOf(max1);
+
+  arr.splice(index, 1);
+
+  for (const num of arr) {
+    max2 = Math.max(num, max2);
+  }
   
-  return (arr.at(-1) -1) * (arr[arr.length - 2] - 1)
+  return (max1 - 1) * (max2 - 1)
 };

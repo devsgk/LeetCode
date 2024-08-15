5476a8d23c9ba6479aad4c548042d8e1fd7439dd
Seunggyu Kim <devkim0702@gmail.com>
2024-08-15 22:10:54 +0800

    Time: 50 ms (81.79%), Space: 50.8 MB (83.07%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.js
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.js
@@ -3,12 +3,17 @@
  * @return {number}
  */
 var countNegatives = function(grid) {
-  const flatted = grid.flat();
+  const m = grid.length;
+  const n = grid[0].length;
   let count = 0;
   
-  for (const num of flatted) {
-    if (num < 0) {
-      count++;
+  for (let i = 0; i < m; i++) {
+    const arr = grid[i];
+    
+    for (let j = 0; j < n; j++) {
+      if (arr[j] < 0) {
+        count++;
+      }
     }
   }
   

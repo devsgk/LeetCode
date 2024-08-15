90cbfbcc9d6f453d5a206db34759cfb1515e095c
Seunggyu Kim <devkim0702@gmail.com>
2024-08-15 23:04:49 +0800

    Time: 56 ms (66.60%), Space: 49.9 MB (71.34%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -3,27 +3,17 @@
  * @return {boolean}
  */
 var isValid = function (s) {
-  const length = s.length;
+    const sLength = s.length;
+    if (sLength % 2 !== 0) return false;
 
-  if (length % 2 !== 0) {
-    return false;
-  }
-
-  const brackets = {
-    "(": ")",
-    "{": "}",
-    "[": "]",
-  };
-
-  const stack = [];
-
-  for (let i = 0; i < length; i++) {
-    if (brackets[s[i]]) {
-      stack.push(s[i]);
-    } else if (s[i] !== brackets[stack.pop()]) {
-      return false;
+    const brackets = {'(': ')', '{': '}', '[': ']'};
+    const stack = [];
+    for (let i = 0; i < sLength; i++) {
+        if (brackets[s[i]]) {
+            stack.push(s[i]);
+        } else if (s[i] !== brackets[stack.pop()]) {
+            return false;
+        }
     }
-  }
-
-  return stack.length === 0;
+    return stack.length === 0;
 };

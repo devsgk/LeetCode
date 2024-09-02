9afcf23fe2681be202370238bb0ed448a6b007d6
Seunggyu Kim <devkim0702@gmail.com>
2024-09-02 23:31:47 +0800

    Time: 66 ms (42.48%), Space: 51.7 MB (17.72%) - LeetHub

--- 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.js
+++ 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.js
@@ -6,7 +6,7 @@ var canAliceWin = function(nums) {
   const single = nums.filter(num => num < 10);
   const double = nums.filter(num => num >= 10);
   const singleSum = single.reduce((acc, cur) => acc + cur, 0);
-  const doubleSum = double.reduce((acc, cur) => acc + cur, 0)
+  const doubleSum = double.reduce((acc, cur) => acc + cur, 0);
   
   return singleSum !== doubleSum;
 };

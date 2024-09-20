4e3cde69348aba1ace5fce05fc6a9aae89455f8a
Seunggyu Kim <devkim0702@gmail.com>
2024-09-20 20:34:28 +0800

    Time: 54 ms (79.17%), Space: 51 MB (20.52%) - LeetHub

--- 3190-find-minimum-operations-to-make-all-elements-divisible-by-three/3190-find-minimum-operations-to-make-all-elements-divisible-by-three.js
+++ 3190-find-minimum-operations-to-make-all-elements-divisible-by-three/3190-find-minimum-operations-to-make-all-elements-divisible-by-three.js
@@ -6,11 +6,9 @@ var minimumOperations = function(nums) {
   let count = 0;
   
   for (const num of nums) {
-    if (num % 3 === 0) continue;
-    
-    const diff = 3 - (num % 3);
-    
-    count = count + Math.min(1, diff);
+    if (num % 3 !== 0) {
+      count++;
+    }
   }
   
   return count;

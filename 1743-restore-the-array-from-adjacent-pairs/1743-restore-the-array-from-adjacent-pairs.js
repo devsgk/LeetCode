edb10a59189cb317e55dc70fd5eb24540a374db1
Seunggyu Kim <devkim0702@gmail.com>
2024-10-07 20:55:41 +0800

    Time: 374 ms (36.96%), Space: 101.3 MB (41.30%) - LeetHub

---
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number[][]} adjacentPairs
+ * @return {number[]}
+ */
+var restoreArray = function(adjacentPairs) {
+  const adj = new Map();
+  
+  for (const [a, b] of adjacentPairs) {
+    if (!adj.has(a)) adj.set(a, []);
+    if (!adj.has(b)) adj.set(b, []);
+    
+    adj.get(a).push(b);
+    adj.get(b).push(a);
+  }
+  
+  console.log(adj);
+  let start;
+  
+  for (const [key, value] of adj) {
+    if (value.length === 1) {
+      start = key;
+      break;
+    }
+  }
+  
+  const queue = [start];
+  const answer = [];
+  const visited = new Set();
+  
+  while (queue.length) {
+    const node = queue.shift();
+    answer.push(node);
+    visited.add(node);
+    
+    for (const neighbor of adj.get(node)) {
+      if (!visited.has(neighbor)) {
+        queue.push(neighbor);
+      }
+    }
+  }
+  
+  return answer;
+};

68284eabb8fab92883656fc059d7361b5f0080ac
Seunggyu Kim <devkim0702@gmail.com>
2024-10-08 20:39:16 +0800

    Time: 80 ms (41.11%), Space: 56.8 MB (8.89%) - LeetHub

---
+++ 0811-subdomain-visit-count/0811-subdomain-visit-count.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string[]} cpdomains
+ * @return {string[]}
+ */
+var subdomainVisits = function(cpdomains) {
+  const obj = {};
+
+  for (const domain of cpdomains) {
+    const [count, address] = domain.split(" ");
+    const subAddress = address.split(".");
+
+    // Iterate through subdomains and accumulate counts
+    for (let i = 0; i < subAddress.length; i++) {
+      const sub = subAddress.slice(i).join(".");
+      obj[sub] = (obj[sub] || 0) + Number(count);
+    }
+  }
+  
+  const result = [];
+  
+  // Convert the counts into the required format
+  for (const [key, value] of Object.entries(obj)) {
+    result.push(`${value} ${key}`);
+  }
+
+  return result;
+};

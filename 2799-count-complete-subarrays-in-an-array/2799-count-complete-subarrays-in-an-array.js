ad3ba2e7c69f6c7bac1ec64b7fef870e4e2716b4
Seunggyu Kim <devkim0702@gmail.com>
2024-10-12 10:48:59 +0800

    Time: 94 ms (71.43%), Space: 54.3 MB (100.00%) - LeetHub

--- 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.js
+++ 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.js
@@ -5,23 +5,22 @@
 var countCompleteSubarrays = function(nums) {
   const size = new Set(nums).size;
   let count = 0;
+  let left = 0;
+  const map = new Map();
   
-  for (let i = 0; i < nums.length; i++) {
-    const map = new Map();
-    let uniqueCount = 0;
+  for (let right = 0; right < nums.length; right++) {
+    map.set(nums[right], (map.get(nums[right]) || 0) + 1);
     
-    for (let j = i; j < nums.length; j++) {
-      if (map.has(nums[j])) {
-        map.set(nums[j], map.get(nums[j]) + 1);
-      } else {
-        map.set(nums[j], 1);
-        uniqueCount++;
+    while (map.size === size) {
+      count += nums.length - right;
+      
+      map.set(nums[left], map.get(nums[left]) - 1);
+      
+      if (map.get(nums[left]) === 0) {
+        map.delete(nums[left]);
       }
       
-      if (uniqueCount === size) {
-        count += nums.length - j;
-        break;
-      }
+      left++;
     }
   }
   

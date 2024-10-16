7b8021a94ac132383cb52bc7b4607f88e1b3d2f8
Seunggyu Kim <devkim0702@gmail.com>
2024-10-16 21:00:06 +0800

    Time: 60 ms (33.64%), Space: 49.1 MB (65.71%) - LeetHub

--- 3274-check-if-two-chessboard-squares-have-the-same-color/3274-check-if-two-chessboard-squares-have-the-same-color.js
+++ 3274-check-if-two-chessboard-squares-have-the-same-color/3274-check-if-two-chessboard-squares-have-the-same-color.js
@@ -4,18 +4,10 @@
  * @return {boolean}
  */
 var checkTwoChessboards = function(coordinate1, coordinate2) {
-  const odd = ["a", "c", "e", "g"];
-  const even = ["b", "d", "f", "h"];
-  const char1 = coordinate1[0];
-  const char2 = coordinate2[0];
-  const num1 = coordinate1[1];
-  const num2 = coordinate2[1];
-  const char1Condition = odd.includes(char1) && odd.includes(char2);
-  const char2Condition = even.includes(char1) && even.includes(char2);
+  const charCode1 = coordinate1[0].charCodeAt(0) - 97;
+  const charCode2 = coordinate2[0].charCodeAt(0) - 97;
+  const row1 = coordinate1[1];
+  const row2 = coordinate2[1];
   
-  if (char1Condition || char2Condition) {
-    return num1 % 2 === num2 % 2;
-  } else {
-    return num1 % 2 !== num2 % 2;
-  }
+  return (Number(charCode1) + Number(row1)) % 2 === (Number(charCode2) + Number(row2)) % 2;
 };

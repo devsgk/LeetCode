381231843f5003b7fa33f20557de6949f255c4b2
Seunggyu Kim <devkim0702@gmail.com>
2024-10-30 20:47:31 +0800

    Time: 7 ms (17.14%), Space: 53.9 MB (77.09%) - LeetHub

---
+++ 2586-count-the-number-of-vowel-strings-in-range/2586-count-the-number-of-vowel-strings-in-range.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} words
+ * @param {number} left
+ * @param {number} right
+ * @return {number}
+ */
+var vowelStrings = function(words, left, right) {
+  const vowels = ["a", "e", "i", "o", "u"];
+  let count = 0;
+  const array = words.slice(left, right + 1);
+  for (const word of array) {
+    const start = word[0];
+    const last = word.at(-1);
+    
+    if (vowels.indexOf(start) === -1 || vowels.indexOf(last) === -1) {
+      continue;
+    } else {
+      count++;
+    }
+  }
+  
+  return count;
+};

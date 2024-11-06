5428d725f12ba45ae2237b9c5fb9a401ed69530b
Seunggyu Kim <devkim0702@gmail.com>
2024-11-06 20:34:34 +0800

    Time: 6 ms (18.00%), Space: 51.8 MB (13.21%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.js
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.js
@@ -3,7 +3,7 @@
  * @return {boolean}
  */
 var halvesAreAlike = function(s) {
-  const vowels = ["a", "e", "i", "o", "u"];
+  const vowels = new Set(["a", "e", "i", "o", "u"]);
   const first = s.slice(0, s.length / 2);
   const second = s.slice(s.length / 2);
   let count1 = 0;
@@ -13,8 +13,8 @@ var halvesAreAlike = function(s) {
     const candidate1 = first[i];
     const candidate2 = second[i];
     
-    if (vowels.includes(candidate1.toLowerCase())) count1++;
-    if (vowels.includes(candidate2.toLowerCase())) count2++;
+    if (vowels.has(candidate1.toLowerCase())) count1++;
+    if (vowels.has(candidate2.toLowerCase())) count2++;
   }
   
   return count1 === count2;

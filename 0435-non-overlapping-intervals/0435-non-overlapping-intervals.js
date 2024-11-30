5a0174d0fb7773762b593fd195830d61add5a96e
Seunggyu Kim <devkim0702@gmail.com>
2024-11-30 13:35:34 +0800

    Time: 74 ms (71.33%), Space: 79.6 MB (79.77%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.js
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.js
@@ -3,19 +3,17 @@
  * @return {number}
  */
 var eraseOverlapIntervals = function(intervals) {
-  const sortedIntervals = intervals.sort((a, b) => a[1] - b[1] || b[0] - a[0]);
+  const sortedIntervals = intervals.sort((a, b) => a[1] - b[1]);
   let count = 0;
   let left = sortedIntervals[0][0];
   let right = sortedIntervals[0][1];
 
   for (let i = 1; i < sortedIntervals.length; i++) {
-    const curLeft = sortedIntervals[i][0];
-    const curRight = sortedIntervals[i][1];
+    const [curLeft, curRight] = sortedIntervals[i];
     
     if (curLeft < right) {
       count++;
     } else {
-      left = curLeft;
       right = curRight;
     }
   }

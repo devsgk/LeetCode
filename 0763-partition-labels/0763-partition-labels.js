306e0aa40181c691374fb71f8f0a3171f7903d48
Seunggyu Kim <devkim0702@gmail.com>
2024-11-30 13:50:55 +0800

    Time: 6 ms (67.02%), Space: 51.9 MB (32.45%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.js
+++ 0763-partition-labels/0763-partition-labels.js
@@ -3,24 +3,24 @@
  * @return {number[]}
  */
 var partitionLabels = function(s) {
-  const obj = {};
+  const result = []
+  const lastIndices = {};
+  let max = 0;
   
   for (let i = 0; i < s.length; i++) {
-    obj[s[i]] = i;
+    lastIndices[s[i]] = i;
   }
   
-  const result = [];
-  let start = 0;
-  let end = 0;
-  
   for (let i = 0; i < s.length; i++) {
-    const cur = s[i];
-
-    end = Math.max(end, obj[cur]);
+    const curChar = s[i];
+    const curCharLastIndex = lastIndices[curChar];
     
-    if (i === end) {
-      result.push(end - start + 1);
-      start = i + 1;
+    max = Math.max(max, curCharLastIndex);
+    
+    if (i === max) {
+      const prevIndex = result.reduce((acc, cur) => acc + cur, 0);
+      
+      result.push(i + 1 - prevIndex);
     }
   }
   

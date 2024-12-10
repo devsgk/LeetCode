25cdd3cea1736ba6651f2bad877d088d5c2f2d2f
Seunggyu Kim <devkim0702@gmail.com>
2024-12-10 23:00:20 +0800

    Time: 2699 ms (31.70%), Space: 17.8 MB (7.22%) - LeetHub

--- 2644-find-the-maximum-divisibility-score/2644-find-the-maximum-divisibility-score.py
+++ 2644-find-the-maximum-divisibility-score/2644-find-the-maximum-divisibility-score.py
@@ -1,15 +1,15 @@
 class Solution:
     def maxDivScore(self, nums: List[int], divisors: List[int]) -> int:
-        result = []
-        
+        list = []
+        divisors.sort()
+
         for divisor in divisors:
           count = 0
-        
+          
           for num in nums:
             if num % divisor == 0: count += 1
           
-          result.append([divisor, count])
-
-        result.sort(key = lambda x: (-x[1], x[0]))
+          list.append(count)
         
-        return result[0][0]
+        return divisors[list.index(max(list))]
+      

4891988c6e31a4b8ddced486cc4a9065f66244e5
Seunggyu Kim <devkim0702@gmail.com>
2024-12-14 14:42:45 +0800

    Time: 134 ms (77.18%), Space: 28.1 MB (15.95%) - LeetHub

---
+++ 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.py
@@ -0,0 +1,15 @@
+class Solution:
+  def countNicePairs(self, nums: List[int]) -> int:
+    MOD = 10 ** 9 + 7
+    arr = [num - int(str(num)[::-1]) for num in nums]
+    count = 0
+    freq = {}
+    
+    for cur in arr:
+      if cur in freq:
+        count = (count + freq[cur]) % MOD
+      
+      freq[cur] = freq.get(cur, 0) + 1
+      
+    return count
+  

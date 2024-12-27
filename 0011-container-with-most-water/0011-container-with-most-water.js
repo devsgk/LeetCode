5212636000dbce84eaad5bc311d33ff8c16e1f4f
Seunggyu Kim <devkim0702@gmail.com>
2024-12-27 17:46:29 +0800

    Time: 3 ms (52.00%), Space: 58.2 MB (23.32%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -3,22 +3,19 @@
  * @return {number}
  */
 var maxArea = function(height) {
-  let max = 0;
   let left = 0;
   let right = height.length - 1;
+  let max = 0;
   
   while (left < right) {
-    let smaller = Math.min(height[left], height[right]);
-    let area = (right - left) * smaller;
+    const area = (right - left) * Math.min(height[left], height[right]);
     
-    if (area >= max) {
-      max = area;
-    }
+    max = Math.max(max, area);
     
-    if (height[left] < height[right]) {
-      left++;
-    } else {
+    if (height[left] > height[right]) {
       right--;
+    } else {
+      left++;
     }
   }
   

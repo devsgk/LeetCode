effbe4c9fd05504cd833faa445ec4b9e3c0edca1
Seunggyu Kim <devkim0702@gmail.com>
2025-01-25 04:11:17 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -0,0 +1,20 @@
+class Solution:
+  def tribonacci(self, n: int) -> int:
+    if n <= 1:
+      return 0
+
+    if n == 1 or n == 2:
+      return 1
+
+    first = 0
+    second = 1
+    third = 1
+    
+    for i in range(3, n + 1):
+      temp = first + second + third
+      
+      first = second
+      second = third
+      third = temp
+    
+    return third

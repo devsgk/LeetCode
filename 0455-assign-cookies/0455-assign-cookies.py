b9711350b3042996316557dbe730fca9b6262475
Seunggyu Kim <devkim0702@gmail.com>
2025-02-12 21:06:21 +0800

    Time: 22 ms (57.46%), Space: 20 MB (27.31%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -0,0 +1,15 @@
+class Solution:
+  def findContentChildren(self, g: List[int], s: List[int]) -> int:
+    g.sort()
+    s.sort()
+
+    child_index = 0
+    cookie_index = 0
+
+    while child_index < len(g) and cookie_index < len(s):
+      if g[child_index] <= s[cookie_index]:
+        child_index += 1
+
+      cookie_index += 1
+
+    return child_index

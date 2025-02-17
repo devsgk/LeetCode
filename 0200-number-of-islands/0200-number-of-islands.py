58fd45641b960298376ef59a784357953979afa5
Seunggyu Kim <devkim0702@gmail.com>
2025-02-17 20:42:01 +0800

    Time: 238 ms (79.54%), Space: 20 MB (82.77%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,23 +1,28 @@
 class Solution:
   def numIslands(self, grid: List[List[str]]) -> int:
     count = 0
-    
-    def dfs(i, j):
-      if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == "0":
-        return
-      
-      grid[i][j] = "0"
+    max_row = len(grid) - 1
+    max_col = len(grid[0]) - 1
 
-      dfs(i + 1, j)
-      dfs(i - 1, j)
-      dfs(i, j + 1)
-      dfs(i, j - 1)
+    def bfs(row, col):
+      grid[row][col] = "0"
+      queue = deque([(row, col)])
+
+      while queue:
+        row, col = queue.popleft()
+
+        for x, y in [(0, 1), (0, -1), (-1, 0), (1, 0)]:
+          new_row, new_col = row + x, col + y
+
+          if 0 <= new_row <= max_row and 0 <= new_col <= max_col and grid[new_row][new_col] == "1":
+            grid[new_row][new_col] = "0"
+            queue.append((new_row, new_col))
 
     for i in range(0, len(grid)):
       for j in range(0, len(grid[0])):
         if grid[i][j] == "1":
           count += 1
 
-          dfs(i, j)
-    
+          bfs(i, j)
+
     return count

4efdc1cc47da08863633656716c28b2b309e10f3
Seunggyu Kim <devkim0702@gmail.com>
2025-03-15 14:17:38 +0800

    Time: 0 ms (100%), Space: 53.8 MB (39.83%) - LeetHub

--- 2490-circular-sentence/2490-circular-sentence.js
+++ 2490-circular-sentence/2490-circular-sentence.js
@@ -4,28 +4,22 @@
  */
 var isCircularSentence = function(sentence) {
   const arr = sentence.split(" ");
-  const result = [];
+  const first = arr[0];
+  const last = arr[arr.length - 1]
+  const middle = arr.slice(1, arr.length);
   
-  for (const word of arr) {
-    const start = word[0];
+  if (first[0] !== last.at(-1)) return false;
+  
+  let start = first.at(-1);
+
+  for (let i = 0; i < middle.length; i++) {
+    const word = middle[i];
+    const first = word[0];
     const last = word.at(-1);
     
-    result.push(start);
-    result.push(last);
-  }
-  
-  for (let i = 0; i < result.length; i++) {
-    if (i === 0) {
-      if (result[i] !== result.at(-1)) {
-        return false;
-      }
-    } else if (i !== result.length - 1) {
-      if (result[i] !== result[i + 1]) {
-        return false;
-      }
-
-      i ++;
-    }
+    if (start !== first) return false;
+    
+    start = last;
   }
   
   return true;

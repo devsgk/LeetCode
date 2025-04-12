307f1d3f74d6276a7eec4236884cb5465218735d
Seunggyu Kim <devkim0702@gmail.com>
2025-04-12 23:33:08 +0800

    Time: 7 ms (69.67%), Space: 18.3 MB (22.43%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
@@ -2,14 +2,7 @@ class Solution:
   def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
     unique1 = set(nums1)
     unique2 = set(nums2)
-    result = [[], []]
+    diff1 = list(unique1 - unique2)
+    diff2 = list(unique2 - unique1)
 
-    for num in unique1:
-      if num not in unique2:
-        result[0].append(num)
-
-    for num in unique2:
-      if num not in unique1:
-        result[1].append(num)
-
-    return result
+    return [diff1, diff2]

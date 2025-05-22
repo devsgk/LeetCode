1dfb668b71798d4a333e7c02696a2f4b35887634
Seunggyu Kim <devkim0702@gmail.com>
2025-05-22 22:33:48 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1037-valid-boomerang/1037-valid-boomerang.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[][]} points
+ * @return {boolean}
+ */
+var isBoomerang = function(points) {
+  const [first, second, third] = [...points];
+  console.log(first, second, third)
+  
+  const slope1 =  (first[1] - second[1]) / (first[0] - second[0]);
+  const slope2 = (first[1] - third[1]) / (first[0] - third[0]);
+  const slope3 = (second[1] - third[1]) / (second[0] - third[0]);
+  
+  return slope1 !== slope2 && slope2 !== slope3 && slope1 !== slope3;
+};

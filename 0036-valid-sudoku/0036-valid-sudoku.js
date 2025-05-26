aebeeb494250622ac7e217970cba3bb5eefc11ea
Seunggyu Kim <devkim0702@gmail.com>
2025-05-26 18:19:21 +0800

    Time: 4 ms (78.82%), Space: 57.1 MB (64.78%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.js
+++ 0036-valid-sudoku/0036-valid-sudoku.js
@@ -3,52 +3,35 @@
  * @return {boolean}
  */
 var isValidSudoku = function(board) {
-  const board2 = [];
-  const board3 = Array.from({ length: 9 }, () => []);
-
-  function check(board) {
-    let condition = true;
-
-    for (const arr of board) {
-      const set = new Set();
-
-      for (let j = 0; j < arr.length; j++) {
-        const num = arr[j];
-
-        if (num !== ".") set.add(num);
-      }
-
-      const length = arr.filter(el => el !== ".").length;
-
-      if (set.size !== length) {
-        condition = false;
-
-        return condition;
-      }
-    }
-
-    return condition;
-  }
-
-  
-
   for (let i = 0; i < 9; i++) {
-    for (let j = 0; j < 9; j++) {
-      const boxIndex = Math.floor(i / 3) * 3 + Math.floor(j / 3);
-
-      board3[boxIndex].push(board[i][j]);
-    }
-  }
-
-  for (let i = 0; i < 9; i++) {
-    const col = [];
+    let rowSet = new Set();
+    let colSet = new Set();
+    let boxSet = new Set();
 
     for (let j = 0; j < 9; j++) {
-      col.push(board[j][i])
-    }
+      let row = board[i][j];
+      let col = board[j][i];
+      let box = board[3 * Math.floor(i / 3) + Math.floor(j / 3)][3 * (i % 3)+(j % 3)];
 
-    board2.push(col);
+      if (row !== ".") {
+        if (rowSet.has(row)) return false;
+
+        rowSet.add(row);
+      }
+
+      if (col !== ".") {
+        if (colSet.has(col)) return false;
+
+        colSet.add(col);
+      }
+
+      if (box !== ".") {
+        if (boxSet.has(box)) return false;
+
+        boxSet.add(box);
+      }
+    }
   }
   
-  return check(board) && check(board2) && check(board3);
+  return true;
 };

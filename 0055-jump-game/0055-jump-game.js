1175e8588e19fbde3da6a4aec746ed3f2268aead
Seunggyu Kim <devkim0702@gmail.com>
2025-06-06 19:00:42 +0900

    Time: 2 ms (54.92%), Space: 58.8 MB (61.99%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+  let maxJump = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i > maxJump) return false;
+
+    maxJump = Math.max(maxJump, i + nums[i]);
+  }
+
+  return true;
+};

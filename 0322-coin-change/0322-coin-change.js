f6d7576233eff594b17659ee219ebbc9e690689c
Seunggyu Kim <devkim0702@gmail.com>
2025-08-10 23:50:29 +0900

    Time: 37 ms (62.32%), Space: 59.7 MB (64.69%) - LeetHub

--- 0322-coin-change/0322-coin-change.js
+++ 0322-coin-change/0322-coin-change.js
@@ -5,9 +5,9 @@
  */
 var coinChange = function(coins, amount) {
   const dp = Array(amount + 1).fill(Infinity);
-  
+
   dp[0] = 0;
-  
+
   for (let i = 1; i <= amount; i++) {
     for (const coin of coins) {
       if (i - coin >= 0) {

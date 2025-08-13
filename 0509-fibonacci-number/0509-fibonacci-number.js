f2003eda0018c9724a26d959279912ca5d2c9b51
Seunggyu Kim <devkim0702@gmail.com>
2025-08-13 18:01:43 +0900

    Time: 55 ms (54.97%), Space: 54.8 MB (11.45%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -3,8 +3,14 @@
  * @return {number}
  */
 var fib = function(n) {
-  if (n === 0) return 0;
-  if (n === 1) return 1;
-  
-  return fib(n - 1) + fib(n - 2);
+  const dp = new Array(n + 1).fill(0);
+
+  dp[0] = 0;
+  dp[1] = 1;
+
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+
+  return dp[n];
 };

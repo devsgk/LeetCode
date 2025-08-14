128c85659af28d00578edbdf0f22445b7649f827
Seunggyu Kim <devkim0702@gmail.com>
2025-08-14 18:33:27 +0900

    Time: 0 ms (100%), Space: 65.1 MB (51.78%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -2,18 +2,30 @@
  * @param {number[]} prices
  * @return {number}
  */
+// var maxProfit = function(prices) {
+//   let buy = prices[0];
+//   let max = 0;
+
+//   for (let i = 1; i < prices.length; i++) {
+//     const cur = prices[i];
+//     let profit = cur - buy;
+
+//     if (profit > 0) max = Math.max(max, profit);
+
+//     buy = Math.min(buy, cur);
+//   }
+
+//   return max;
+// };
+
 var maxProfit = function(prices) {
-  let buy = prices[0];
-  let max = 0;
+  let min = Infinity;
+  let maxProfit = -Infinity;
 
-  for (let i = 1; i < prices.length; i++) {
-    const cur = prices[i];
-    let profit = cur - buy;
-
-    if (profit > 0) max = Math.max(max, profit);
-
-    buy = Math.min(buy, cur);
+  for (let i = 0; i < prices.length - 1; i++) {
+    min = Math.min(min, prices[i]);
+    maxProfit = Math.max(prices[i + 1] - min, maxProfit);
   }
 
-  return max;
-};
+  return maxProfit > 0 ? maxProfit : 0
+}

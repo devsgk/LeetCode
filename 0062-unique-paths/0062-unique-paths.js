35303feb9e857bc1d8dd595a91320a8bf35f49e2
Seunggyu Kim <devkim0702@gmail.com>
2025-08-15 16:06:41 +0900

    Time: 0 ms (100%), Space: 53.8 MB (61%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.js
+++ 0062-unique-paths/0062-unique-paths.js
@@ -3,8 +3,24 @@
  * @param {number} n
  * @return {number}
  */
+// var uniquePaths = function(m, n) {
+//   const dp = Array.from({ length: m }, () => Array(n).fill(1));
+
+//   for (let i = 1; i < m; i++) {
+//     for (let j = 1; j < n; j++) {
+//       dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+//     }
+//   }
+
+//   return dp[m - 1][n - 1];
+// };
+
 var uniquePaths = function(m, n) {
-  const dp = Array.from({ length: m }, () => Array(n).fill(1));
+  const dp = new Array(m);
+
+  for (let i = 0; i < dp.length; i++) {
+    dp[i] = new Array(n).fill(1);
+  }
 
   for (let i = 1; i < m; i++) {
     for (let j = 1; j < n; j++) {

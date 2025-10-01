79d892cd4b420f0556b0423cbeddbf8f545c3239
Seunggyu Kim <devkim0702@gmail.com>
2025-10-01 17:12:11 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.js
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.js
@@ -3,28 +3,16 @@
  * @return {number}
  */
 var maxDistance = function(colors) {
-  const obj = {};
-
-  for (let i = 0; i < colors.length; i++) {
-    const cur = colors[i];
-
-    for (let j = 0; j < colors.length; j++) {
-      if (i !== j) {
-        const next = colors[j];
-        
-        if (cur !== next) {
-          const diff = Math.abs(i - j);
-
-          obj[cur] = obj[cur] ? Math.max(diff, obj[cur]) : diff;
-        }
-      }
-    }
-  }
-
   let max = 0;
 
-  for (const [key, value] of Object.entries(obj)) {
-    max = Math.max(max, value);
+  for (let i = 0; i < colors.length; i++) {
+    if (colors[i] !== colors[0]) {
+      max = Math.abs(i - 0);
+    }
+    
+    if (colors[i] !== colors[colors.length - 1]) {
+      max = Math.abs(i - (colors.length - 1));
+    }
   }
 
   return max;

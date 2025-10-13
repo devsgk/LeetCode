0c3afa3a173adda88ea39d7f075e2b32d6fa7ac7
Seunggyu Kim <devkim0702@gmail.com>
2025-10-13 13:04:34 +0300

    Time: 16 ms (5%), Space: 60.4 MB (5.71%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.js
+++ 0859-buddy-strings/0859-buddy-strings.js
@@ -4,36 +4,38 @@
  * @return {boolean}
  */
 var buddyStrings = function(s, goal) {
-  if (s.length !== goal.length) {
-    return false;
-  }
-  
-  const arr = [];
-  
+  const freq1 = {};
+  const freq2 = {};
+
+  if (s.length !== goal.length) return false;
+
+  let count = 0;
+
   for (let i = 0; i < s.length; i++) {
-    const first = s[i];
-    const second = goal[i];
-    
-    if (first !== second) {
-      arr.push(i);
-    }
+    if (s[i] !== goal[i]) count++;
   }
 
-  if (arr.length === 2) {
-    const [a, b] = arr;
-    
-    if (s[a] === goal[b] && s[b] === goal[a]) {
-      return true;
-    }
-  } else if (arr.length === 0) {
-    const sArr = s.split("");
-    const set = new Set(sArr);
+  if (count > 2) return false;
 
-    if (set.size < s.length) {
-
-      return true;
-    }
+  for (const letter of s) {
+    freq1[letter] = freq1[letter] ? freq1[letter] + 1 : 1;
   }
-  
-  return false;
+
+  for (const letter of goal) {
+    freq2[letter] = freq2[letter] ? freq2[letter] + 1 : 1;
+  }
+
+  if (s === goal) {
+    if (Object.values(freq1).some(value => value >= 2)) return true;
+    else return false;
+  }
+
+  for (const [key, value] of Object.entries(freq1)) {
+    console.log(key, value)
+    console.log(freq2[key])
+    if (value === freq2[key]) continue;
+    else return false;
+  }
+
+  return true;
 };

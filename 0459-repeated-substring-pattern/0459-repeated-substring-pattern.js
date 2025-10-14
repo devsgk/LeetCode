8bcb84f382e2090a230b562ec8518eb2e7242fec
Seunggyu Kim <devkim0702@gmail.com>
2025-10-14 12:03:40 +0300

    Time: 361 ms (5.05%), Space: 62.4 MB (10.55%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.js
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.js
@@ -3,8 +3,23 @@
  * @return {boolean}
  */
 var repeatedSubstringPattern = function(s) {
-  const duplicate = s + s;
-  const modified = duplicate.slice(1, -1);
-  
-  return modified.includes(s);  
+  let subString = "";
+
+  for (let i = 0; i < s.length; i++) {
+    subString += s[i];
+
+    let count = 2;
+    let newSubstring = subString;
+
+
+    while (newSubstring.length <= s.length) {
+      newSubstring = subString.repeat(count);
+
+      if (newSubstring === s) return true;
+
+      count++;
+    }
+  }
+
+  return false;
 };

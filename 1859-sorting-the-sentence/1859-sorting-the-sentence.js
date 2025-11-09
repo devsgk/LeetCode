5d3dc9876607b936070d23fe42feeb6bccd5ef6f
Seunggyu Kim <devkim0702@gmail.com>
2025-11-09 22:50:12 +0800

    Time: 52 ms (21.48%), Space: 53.7 MB (74.44%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.js
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.js
@@ -3,17 +3,9 @@
  * @return {string}
  */
 var sortSentence = function(s) {
-  const array = s.split(" ");
-  
-  array.sort((a, b) => a.at(-1) - b.at(-1));
-  
-  const resultArray = array.map(word => {
-    return word.slice(0, -1);
-  })
-  
-  let str = "";
-  
-  resultArray.forEach(word => str += " " + word);
-  
-  return str.trim();
+  const arr = s.split(" ");
+  const sorted = arr.sort((a, b) => a.at(-1) - b.at(-1));
+  const result = sorted.map(word => word.slice(0, word.length - 1));
+
+  return result.join(" ");
 };
